5f54104bb3e3525d0ce958b5b541952c8a417441
UlfathAraIslam <mouliajsahid@gmail.com>
2025-03-02 15:45:23 +0900

    integrated completed btn functionally

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+const completeBtns = document.querySelectorAll(".complete-btn");
+completeBtns.forEach(function(completeBtn){
+    completeBtn.addEventListener("click",function(event){
+        alert("Board updated successfully");
+
+        completeBtn.classList.add("!bg-indigo-200", "text-white");
+        completeBtn.disabled =true;
+
+        const assignedTaskCount = document.getElementById("assigned-task-count").innerText;
+        let convertedAssignedTaskCount = parseInt(assignedTaskCount);
+
+        if (assignedTaskCount > 0){
+            convertedAssignedTaskCount--;
+        }
+        // TODO: to show the last complete btn 2 alert
+        if (assignedTaskCount === 0){
+            alert("Congrats!! You have completed all the current tasks!")
+        }
+
+        document.getElementById("assigned-task-count").innerText = convertedAssignedTaskCount;
+
+        const completeTaskCount = document.getElementById("complete-task-count").innerText;
+        let convertedCompleteTaskCount = parseInt(completeTaskCount);
+        if (completeTaskCount >=0){
+            convertedCompleteTaskCount++
+        }
+        document.getElementById("complete-task-count").innerText = convertedCompleteTaskCount;
+
+        const historyContainer = document.getElementById("task-complete-history");
+        const historyItem = document.createElement("div");
+        historyItem.classList.add("bg-blue-50", "rounded-lg", "p-3", "m-5","text-gray-500") ;
+        const cardTitle =completeBtn.closest(".card").querySelector(".card-title").innerText;
+        const now = new Date();
+        const formattedTime = now.toLocaleTimeString();
+        historyItem.innerHTML = `
+        You have Complete The Task <br>${cardTitle} at ${formattedTime}
+        `
+        historyContainer.appendChild(historyItem);
+
+    })
+})
